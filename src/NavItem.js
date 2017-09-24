5fb1fba460445d94cad1bc359301555dc18213be
guher.bozdogan <guher.bozdogan.wrk@gmail.com>
2017-09-25 01:58:02 +0300

    minor fix

--- src/NavItem.js
+++ src/NavItem.js
@@ -11,7 +11,7 @@ const NavItem = ({
   if (divider) return <li className='divider' />;
   return (
     <li {...props}>
-      <a href={href} onClick={onclick} >
+      <a href={href} onclick={onclick} >
         { children }
       </a>
     </li>
